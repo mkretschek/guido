f8fd9c7da8d0ab3ce51cfbe3f8d71b8bd06789cb
Mathias Kretschek <mathias@kretschek.com.br>
2014-06-15 13:23:53 -0300

    Wrote loader() and simplify() basic test cases (not implemented yet)

---
+++ test/helper/load.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  describe('load() helper', function () {
+    it('is accessible');
+    it('is a function');
+
+    it('loads the file at the given path');
+    it('loads all files at the paths in an array');
+    it('loads all files that match the given glob');
+    it('concatenates loaded files into a single object');
+    it('returns an object');
+  });
+})();
---
+++ test/helper/simplify.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict';
+
+  describe('simplify() helper', function () {
+    it('is accessible');
+    it('is a function');
+    it('defaults to using javascript native types');
+    it('simplifies a descriptor object to native types');
+    it('simplifies property descriptors');
+    it('simplifies item descriptors');
+  });
+})();
